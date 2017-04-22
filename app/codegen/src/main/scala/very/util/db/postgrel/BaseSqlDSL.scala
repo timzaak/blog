d878e0c75f86ec2db7023b1caacdcb2b10948ede
timzaak <zsy.evan@gmail.com>
2017-04-22 20:03:35 +0800

    close #6
    1. 修正参数循序错误
    2. 改造graphiql.html

--- app/codegen/src/main/scala/very/util/db/postgrel/BaseSqlDSL.scala
+++ app/codegen/src/main/scala/very/util/db/postgrel/BaseSqlDSL.scala
@@ -9,16 +9,16 @@ trait BaseSqlDSL extends WithPostgrel {
   protected def tableName: String
 
   //danger.
-  protected def paginate[Entity](page: Int = 1,
-              pageSize: Int = 10,
-              whereClause: Option[String] = None,
-              orderBy: Option[String] = None
-             )(implicit v:GetResult[Entity]): Future[(Vector[Entity],Long)] = {
+  protected def paginate[Entity](
+                                  page: Int = 1,
+                                  pageSize: Int = 10,
+                                  whereClause: Option[String] = None,
+                                  orderBy: Option[String] = None
+                                )(implicit v: GetResult[Entity]): Future[(Vector[Entity], Long)] = {
 
     val where = whereClause.map(x => s" where $x").getOrElse("")
-
     db.run(sql"""select * from #$tableName #$where #${orderBy.map(x => s" order by $x").getOrElse("")} limit $pageSize offset ${pageSize * (page - 1)}""".as[Entity]) zip
-    db.run(sql"select count(*) from #$tableName #$where".as[Long].head)
+      db.run(sql"select count(*) from #$tableName #$where".as[Long].head)
   }
 
 }
--- app/src/main/scala/com/timzaak/action/CommentAction.scala
+++ app/src/main/scala/com/timzaak/action/CommentAction.scala
@@ -6,7 +6,7 @@ import com.timzaak.entity.Comment
 trait CommentAction extends Action {
   protected def commentDao: CommentDao
 
-  def myComments(userId: L, pageSize: I, page: I) = commentDao.pages(userId, pageSize, page)
+  def myComments(userId: L, pageSize: I, page: I) = commentDao.pages(userId, page, pageSize)
 
   def postComment(fromId: L, toId: L, content: S) = commentDao.createComment(fromId, toId, content)
 
