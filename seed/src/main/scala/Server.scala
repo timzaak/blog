e5aebedec09d1254496d54a3f5f050090b61c8fb
timzaak <zsy.evan@gmail.com>
2017-11-19 00:34:15 +0800

    fix

--- seed/src/main/scala/Server.scala
+++ seed/src/main/scala/Server.scala
@@ -4,7 +4,6 @@ import com.typesafe.config.ConfigFactory
 
 object Server extends App  {
   val conf = ConfigFactory.load()
-  println("======================",conf.getInt("akka.remote.netty.tcp.port"))
   val system = ActorSystem("app", conf)
   system.actorOf(Props[SimpleClusterListener], name = "clusterListener")
 }
