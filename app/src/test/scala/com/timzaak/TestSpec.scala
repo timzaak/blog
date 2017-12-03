bbb943e2435333299b3da0cfe3bc24086caa4171
timzaak <zsy.evan@gmail.com>
2017-12-03 10:43:46 +0800

    fix test

--- app/src/test/scala/com/timzaak/TestSpec.scala
+++ app/src/test/scala/com/timzaak/TestSpec.scala
@@ -6,14 +6,9 @@ import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{ Millis, Seconds, Span }
 import org.scalatest.{ FreeSpec, Matchers }
 
-trait TestSpec extends FreeSpec with Matchers with WithTestConf with ScalaFutures {
-  implicit val system           = AkkaHelper.system
-  implicit val materializer     = AkkaHelper.materializer
-  implicit val executionContext = AkkaHelper.executionContext
-
+trait TestSpec extends FreeSpec with Matchers with WithTestConf with ScalaFutures with AkkaDI {
   implicit val defaultPatience =
     PatienceConfig(timeout = Span(10, Seconds), interval = Span(1000, Millis))
 
 }
 
-object AkkaHelper extends AkkaDI {}
---
+++ app/src/test/scala/com/timzaak/chaos/SelfTypeTest.scala
@@ -0,0 +1,26 @@
+package com.timzaak.chaos
+
+
+import org.scalatest.{FreeSpec, Matchers}
+
+class SelfTypeTest extends FreeSpec with Matchers{
+  trait Abstract0[+Self <: Abstract0[_]] {
+    def get:this.type = this
+    def getList:List[Self] = List.empty
+  }
+
+  class Concrete0 extends Abstract0[Concrete0] {}
+
+  class Concrete1 extends Abstract0[Concrete1] {}
+
+
+  "Self1 with Abstract0 can work" in {
+    val c0 = new Concrete0().get
+    val c1 = new Concrete1().get
+    val c2 = (c0.getList ::: c0.getList):List[Concrete0]
+    val c3: Iterable[Abstract0[Any]] = c0.getList:::c1.getList
+
+    assert(true, "bad one")
+  }
+
+}
