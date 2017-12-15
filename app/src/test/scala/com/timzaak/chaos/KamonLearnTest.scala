cbc658413880635bdf96bafd2303f668a1d36740
timzaak <zsy.evan@gmail.com>
2017-12-16 00:50:37 +0800

    add kamon log reporter implement

--- app/src/test/scala/com/timzaak/chaos/KamonLearnTest.scala
+++ app/src/test/scala/com/timzaak/chaos/KamonLearnTest.scala
@@ -1,14 +1,23 @@
 package com.timzaak.chaos
 
 
-import kamon.Kamon
-import kamon.logreporter.LogReporter
+import com.typesafe.config.ConfigFactory
+import kamon.{Kamon, MetricReporter, SpanReporter}
 import org.scalatest.{FreeSpec, Matchers}
+import very.util.kamon.KamonLogReporter
 
-class KamonLearnTest extends FreeSpec with Matchers{
+class KamonLearnTest extends FreeSpec with Matchers {
   "kamon" - {
     "counter" in {
-
+      //Kamon.addReporter(KamonLogReporter: SpanReporter, "spanReporter")
+      //Kamon.reconfigure(ConfigFactory.parseString("kamon.metric.tick-interval=1s"))
+      val counter = Kamon.counter("hello")
+      Kamon.addReporter(KamonLogReporter: MetricReporter, "metric")
+      for{ i  <- 1 to 20}{
+        counter.increment()
+        Thread.sleep(1000)
+      }
+      Kamon.stopAllReporters()
       1 shouldBe 1
     }
   }
