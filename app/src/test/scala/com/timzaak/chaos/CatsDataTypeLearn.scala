b43981d917d791f8420e665aeadd284939e63f96
timzaak <zsy.evan@gmail.com>
2018-02-09 00:15:14 +0800

    add cats learn

---
+++ app/src/test/scala/com/timzaak/chaos/CatsDataTypeLearn.scala
@@ -0,0 +1,12 @@
+package com.timzaak.chaos
+
+import org.scalatest.{FreeSpec, Matchers}
+import cats._
+import data._
+import implicits._
+
+class CatsDataTypeLearn  extends FreeSpec with Matchers {
+  "Const" in {
+
+  }
+}
