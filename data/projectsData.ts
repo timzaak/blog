1daa6875dc9a28e784c6d28ba79c20b90e299836
timzaak <zsy.evan@gmail.com>
2025-07-08 14:12:18 +0800

    change main theme

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,39 +8,39 @@ interface Project {
 const projectsData: Project[] = [
   {
     title: 'HP',
-    description: `A tutorial project to help Java Programmer to get the sense of measure the performance of HTTP server`,
+    description: `A tutorial project to help Java programmer to get the sense of measuring the performance of HTTP web server`,
     imgSrc: `/static/images/time-machine.jpg`,
-    href: 'https://timzaak.github.io/hp/',
+    href: 'https://timzaak.github.io/hp',
   },
   {
     title: 'ForNet',
-    description: `It's a enterprise VPN project, based on Rust + Wireguard, support Linux, Windows, macOS OS platform.`,
+    description: `Enterprise VPN project, based on Rust + Wireguard, support Linux, Windows, macOS.`,
     imgSrc: `/static/images/project/fornet.jpeg`,
     href: 'https://github.com/ForNetCode/fornet',
   },
   {
     title: 'Web Sugar',
-    description: `It's a scala web api starter project, including some other projects, like React Admin Web, database code auto generate, wechat pay support.`,
+    description: `Scala web api starter project, including some other projects, like React Admin Web, database code auto generate, wechat pay support.`,
     href: 'https://github.com/ForNetCode/web-sugar-startup',
   },
   {
     title: 'Desktop Message',
-    description: `Mqtt + static file http server + service discovery for desktop app, to interact with devices.`,
+    description: `MQTT + static file http server + service discovery for desktop app interacting with devices.`,
     href: 'https://github.com/timzaak/desktop-message',
   },
   {
     title: 'Log Http',
-    description: `log http/https request/response via MITM, for PC software developer who has none Chrome Console.`,
+    description: `Log http/https request/response via MITM, for PC software developer who has none Chrome Console.`,
     href: 'https://github.com/timzaak/log-http-proxy',
   },
   {
     title: 'Table2Case',
-    description: `convert database table to scala case class and scalaSQL ORM, support PG, MySQL, Sqlite.`,
+    description: `Convert database table to scala case class or scalaSQL ORM, support PG, MySQL, Sqlite.`,
     href: 'https://github.com/timzaak/table2case',
   },
   {
     title: 'DevOps Automation Utilities',
-    description: `scripts about daily devops jobs, including bakeup, docker image transfer, encrypt decrypt file and so on`,
+    description: `Scripts of daily devops jobs, including bake up, docker image transfer, encrypt decrypt file and so on`,
     href: 'https://github.com/timzaak/devops',
   },
 ]
